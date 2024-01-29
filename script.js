c30a1f7c420d8037447ba09666bfb906d019b9af
SvdNeo <sanjay.d@neoito.com>
2024-01-29 17:16:11 +0530

    created function to check for straight

--- script.js
+++ script.js
@@ -101,4 +101,34 @@ const rollDice = () => {
     updateRadioOption(5, 0);
   };
   
- 
+  const checkForStraights = (arr) => {
+    const sortedNumbersArr = arr.sort((a, b) => a - b);
+    const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+    const uniqueNumbersStr = uniqueNumbersArr.join("");
+  
+    const smallStraightsArr = ["1234", "2345", "3456"];
+    const largeStraightsArr = ["12345", "23456"];
+  
+    if (smallStraightsArr.includes(uniqueNumbersStr)) {
+      updateRadioOption(3, 30);
+    }
+  
+    if (largeStraightsArr.includes(uniqueNumbersStr)) {
+      updateRadioOption(4, 40);
+    }
+  
+    updateRadioOption(5, 0);
+  };
+  
+  const resetRadioOption = () => {
+    scoreInputs.forEach((input) => {
+      input.disabled = true;
+      input.checked = false;
+    });
+  
+    scoreSpans.forEach((span) => {
+      span.textContent = "";
+    });
+  };
+  
+  
